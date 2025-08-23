d8613ffe0c9b91c53508fe15b5b6d351d94fc39b
damil27 <dhamilare27@gmail.com>
2025-08-23 00:52:27 -0700

    setup and configured radix theme for better layout and utilities

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
 
 const TicketsPage = () => {
   return (
     <div>
       <div>Ticket pages here</div>
+      <Button>Click me</Button>
     </div>
   );
 };
