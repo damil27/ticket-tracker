f097aa6fede2e8f35b7d835e475df9a4f7cae8f5
damil27 <dhamilare27@gmail.com>
2025-08-23 00:45:42 -0700

    build an api for creating new ticket

---
+++ app/api/tickets/route.tsx
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../lib/prisma";
+import { z } from "zod";
+
+
+
+const CreateTicketsSchema = z.object({
+    title: z.string().min(3).max(255),
+    description: z.string().min(1),
+})
+export async function POST(request: NextRequest) {
+    const body =  await request.json();
+    const validation = CreateTicketsSchema.safeParse(body);
+    if(!validation.success){
+        return NextResponse.json({error: validation.error}, {status: 400});
+    }
+
+    const newTicket  = await prisma.ticket.create({
+        data: {
+            title: validation.data.title,
+            description: validation.data.description,
+        }
+    });
+    return NextResponse.json(newTicket, {status: 201});
+}
---
+++ prisma/migrations/20250823072125_modifcation_to_ticket_datatype/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `ticket` MODIFY `title` VARCHAR(255) NOT NULL,
+    MODIFY `description` TEXT NULL;
