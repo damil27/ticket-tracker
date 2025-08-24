155656bc572ecccda5a19d52b08fbbc245d3fee7
damil27 <dhamilare27@gmail.com>
2025-08-23 20:22:34 -0700

    add a markdone editior for better user experience

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+'use client';
+import { Button, TextField } from '@radix-ui/themes';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 
 const NewTicketPage = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder='Enter a title' />
-      <TextArea placeholder='Enter a description' />
+      <SimpleMDE placeholder='Enter a description' />
       <Button> Submit Ticket</Button>
     </div>
   );
