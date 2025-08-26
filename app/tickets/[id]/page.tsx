264c512b83537bc91e61d14fd06417ab8804cb25
damil27 <dhamilare27@gmail.com>
2025-08-26 08:59:59 -0700

    syle the issue details page

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,4 +1,6 @@
+import TicketBadge from '@/app/components/ticketBadge';
 import { prisma } from '@/app/lib/prisma';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import React from 'react';
 
 const TecketDetailsPage = async ({ params }: { params: { id: string } }) => {
@@ -8,11 +10,14 @@ const TecketDetailsPage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div>
-      <h2>Ticket ID: {ticket?.id}</h2>
-      <p>Ticket title {ticket?.title}</p>
-      <p>Ticket Description : {ticket?.description}</p>
-      <p>Ticket Status : {ticket?.status}</p>
-      <p>Ticket Created At : {ticket?.createdAt.toDateString()}</p>
+      <Heading>Ticket title: {ticket?.title}</Heading>
+      <Flex gap='2' align='center' className='mb-4'>
+        <TicketBadge status={ticket?.status!} />
+        <Text>Ticket Created At : {ticket?.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <Text>Ticket Description : {ticket?.description}</Text>
+      </Card>
     </div>
   );
 };
