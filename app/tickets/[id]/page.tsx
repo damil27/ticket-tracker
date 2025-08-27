7e603cfbed5cb09a2db764fb3ee8f7070230f887
damil27 <dhamilare27@gmail.com>
2025-08-27 03:18:56 -0700

    Revert "syle the issue details page"
    This reverts commit 264c512b83537bc91e61d14fd06417ab8804cb25.

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,6 +1,4 @@
-import TicketBadge from '@/app/components/ticketBadge';
 import { prisma } from '@/app/lib/prisma';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
@@ -11,14 +9,11 @@ const TecketDetailsPage = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div>
-      <Heading>Ticket title: {ticket?.title}</Heading>
-      <Flex gap='2' align='center' className='mb-4'>
-        <TicketBadge status={ticket?.status!} />
-        <Text>Ticket Created At : {ticket?.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose'>
-        <ReactMarkdown>{ticket?.description}</ReactMarkdown>
-      </Card>
+      <h2>Ticket ID: {ticket?.id}</h2>
+      <p>Ticket title {ticket?.title}</p>
+      <p>Ticket Description : {ticket?.description}</p>
+      <p>Ticket Status : {ticket?.status}</p>
+      <p>Ticket Created At : {ticket?.createdAt.toDateString()}</p>
     </div>
   );
 };
